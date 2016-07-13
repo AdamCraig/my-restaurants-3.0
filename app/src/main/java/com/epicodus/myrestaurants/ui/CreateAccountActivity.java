e85784c4371b190ae7d3b2bfb4531d2d0d7bbd67
Adam Craig and Noah Kittleson <adam+noah@>
2016-07-13 14:05:55 -0700

    Add personalized welcome message to AppBar on successful signup

--- app/src/main/java/com/epicodus/myrestaurants/ui/CreateAccountActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/CreateAccountActivity.java
@@ -18,6 +18,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -67,13 +68,13 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
     }
 
     public void createNewUser() {
-        final String name = mNameEditText.getText().toString().trim();
+        mName = mNameEditText.getText().toString().trim();
         final String email = mEmailEditText.getText().toString().trim();
         String password = mPasswordEditText.getText().toString().trim();
         String confirmPassword = mConfirmPasswordEditText.getText().toString().trim();
 
         boolean validEmail = isValidEmail(email);
-        boolean validName = isValidName(name);
+        boolean validName = isValidName(mName);
         boolean validPassword = isValidPassword(password, confirmPassword);
         if (!validEmail || !validName || !validPassword) return;
 
@@ -86,12 +87,30 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
                     mAuthProgressDialog.dismiss();
                     if (task.isSuccessful()) {
                         Log.d(TAG, "Authentication successful!");
+                        createFirebaseUserProfile(task.getResult().getUser());
                     } else {
                         Toast.makeText(CreateAccountActivity.this, "Authentication failed :(",
                                 Toast.LENGTH_SHORT).show();
                     }
                 }
             });
+    }
+
+    private void createFirebaseUserProfile(final FirebaseUser user) {
+
+        UserProfileChangeRequest addProfileName = new UserProfileChangeRequest.Builder()
+                .setDisplayName(mName)
+                .build();
+
+        user.updateProfile(addProfileName)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, user.getDisplayName());
+                        }
+                    }
+                });
     }
 
     private void createAuthStateListener() {
